16e5e5252370cd7e18bae857952ad700458ac334
neolitec <kev.manson@gmail.com>
2020-02-10 23:00:33 -0500

    Clean things.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,10 +4,9 @@
 
 import 'react-native';
 import React from 'react';
+// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
   renderer.create(<App />);
